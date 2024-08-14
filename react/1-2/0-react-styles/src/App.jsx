4ff92c1eb782cf995070675c02399e69f512d5f0
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-08-14 21:57:30 +0200

    React Module 1 Class 2

---
+++ react/1-2/0-react-styles/src/App.jsx
@@ -0,0 +1,23 @@
+import { StyleProp } from "./0-style-prop/StyleProp";
+import { Css } from "./1-css/Css";
+import { ClassComposition } from "./2-class-composition/ClassComposition";
+import { Clsx } from "./3-clsx/Clsx";
+import { Globals } from "./4-globals/Globals";
+import { ReusingStyles } from "./5-reusing-styles/ReusingStyles";
+import { CssModules } from "./6-css-modules/CssModules";
+import { Normalization } from "./7-normalization/Normalization";
+import { ReactIcons } from "./8-react-icons/ReactIcons";
+
+export const App = () => (
+  <>
+    {/* <StyleProp /> */}
+    {/* <Css /> */}
+    {/* <ClassComposition /> */}
+    {/* <Clsx /> */}
+    {/* <Globals /> */}
+    {/* <ReusingStyles /> */}
+    {/* <CssModules /> */}
+    {/* <Normalization /> */}
+    <ReactIcons />
+  </>
+);
